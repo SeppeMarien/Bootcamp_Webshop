c0465500889867403f2a6fa8a32052556ccde57f
Steven De Keuster <stevendek@hotmail.com>
2019-10-28 15:38:36 +0100

    addProduct works

--- src/store/reducers/ProductReducer.js
+++ src/store/reducers/ProductReducer.js
@@ -1,7 +1,19 @@
-// import { ADD_PRODUCT } from '../Actions/actionTypes';
+import { ADD_PRODUCT } from '../Actions/actionTypes';
 
 const initialState = {};
 
-export default function productReducer(state = initialState) {
-  return state;
+export default function productReducer(state = initialState, action) {
+  switch (action && action.type) {
+    case ADD_PRODUCT:
+      if (state[action.payload.id])
+        return {
+          ...state,
+          [action.payload.id]: { product: action.payload, amount: state[action.payload.id].amount + 1 },
+        };
+      return { ...state, [action.payload.id]: { product: action.payload, amount: 1 } };
+    default:
+      return state;
+  }
 }
+
+// return { ...state, [action.payload.id]: { product: action.payload, amount: state[action.payload.id].amount + 1 } };
--- src/store/reducers/ProductReducer.spec.js
+++ src/store/reducers/ProductReducer.spec.js
@@ -1,4 +1,5 @@
 import productReducer from './ProductReducer';
+import { addProduct } from '../Actions/ProductActions';
 // import { ADD_PRODUCT } from '../Actions/actionTypes';
 // import {ADD_PRODUCT, REMOVE_PRODUCT} from "../Actions/actionTypes"
 
@@ -13,4 +14,32 @@ test('returns initial state', () => {
   expect(newState).toEqual({});
 });
 
-test('ADD_PRODUCT', () => {});
+describe('ADD_PRODUCT', () => {
+  test('ADD_PRODUCT adds a product to a new list', () => {
+    const prevState = {};
+    const product = { id: 3, name: 'cola' };
+    const newState = productReducer(prevState, addProduct(product));
+    expect(newState).toEqual({ 3: { product: { id: 3, name: 'cola' }, amount: 1 } });
+  });
+
+  test('ADD_PRODUCT adds a product to an existing list', () => {
+    const prevState = { 1: { product: { id: 1, name: 'fanta' }, amount: 1 } };
+    const product = { id: 3, name: 'cola' };
+    const expectedNewState = {
+      1: { product: { id: 1, name: 'fanta' }, amount: 1 },
+      3: { product: { id: 3, name: 'cola' }, amount: 1 },
+    };
+    const newState = productReducer(prevState, addProduct(product));
+    expect(newState).toEqual(expectedNewState);
+  });
+
+  test('ADD_PRODUCT raises the amount of a product to an existing list', () => {
+    const product = { id: 3, name: 'cola' };
+    const prevState = {
+      1: { product: { id: 1, name: 'fanta' }, amount: 1 },
+      3: { product: { id: 3, name: 'cola' }, amount: 1 },
+    };
+    const newState = productReducer(prevState, addProduct(product));
+    expect(newState[3].amount).toEqual(2);
+  });
+});
