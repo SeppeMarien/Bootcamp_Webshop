cfd6c2c45e546be06abd1df11d4934298f916861
Steven De Keuster <stevendek@hotmail.com>
2019-10-29 08:08:58 +0100

    store updated

--- src/store/store.jsx
+++ src/store/store.jsx
@@ -1,9 +1,11 @@
 import { combineReducers, createStore } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension/logOnlyInProduction';
 import { TodoReducer } from './reducers/TodoReducer';
+import productReducer from './reducers/ProductReducer';
 
 const reducer = combineReducers({
   todos: TodoReducer,
+  products: productReducer,
 });
 
 const store = createStore(reducer, composeWithDevTools());
